7d3499b39c42fdbdc60ace37b08ae5678436030f
zuoerfeng <617376665@qq.com>
2017-12-08 20:39:13 +0800

    update_chapter_2

---
+++ chapter_2/Sales_data/main.cpp
@@ -0,0 +1,46 @@
+/***************************************************/
+//Description:c++ primer chapter2
+//Author: Zuo Lu 		Version: 1.0	Date:2017/12/06
+/***************************************************/
+
+#include "sales_data.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	Sales_data data1, data2;
+	double price = 0;
+
+	//input
+	std::cin >> data1.bookNo >> data1.units_sold >> price;
+	data1.revenue = data1.units_sold * price;
+
+	std::cin >> data2.bookNo >> data2.units_sold >> price;
+	data2.revenue = data2.units_sold * price;
+
+	//calculate
+	if (data1.bookNo == data2.bookNo)
+	{
+		unsigned totalCnt = data1.units_sold + data2.units_sold;
+		double totalRevenue = data1.revenue + data2.revenue;
+
+		std::cout << data1.bookNo << " " << totalCnt
+			<< " " << totalRevenue << " ";
+
+		if (totalCnt != 0)
+			std::cout << totalRevenue / totalCnt << std::endl;
+		else
+			std::cout << "(no sales)" << std::endl;
+
+		return 0;
+	}
+	else
+	{
+		std::cerr << "Data must refer to the same ISBN"
+			<< std::endl;
+
+		return -1;
+	}
+}
+
---
+++ chapter_2/Sales_data/sales_data.h
@@ -0,0 +1,25 @@
+/***************************************************/
+//Description:c++ primer chapter2
+//Author: Zuo Lu 		Version: 1.0	Date:2017/12/06
+/***************************************************/
+
+//#ifdef VARIABLE_NAME
+//    code1
+//#else
+//    code2
+//#endif
+
+
+#ifndef SALES_DATA_H  //当这个SALES_DATA_H没有被定义的时候
+#define SALES_DATA_H  //定义这个SALES_DATA_H文件
+
+#include <string>
+
+struct Sales_data {
+
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+#endif
+
